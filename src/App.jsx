a3a5f98317adc937795c349687cb703cb1455601
Figmauchun <figmauchun123@gmail.com>
2025-09-28 15:27:53 +0500

    add forms element

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,12 @@
-import { Button, Checkbox, Label, TextInput } from "flowbite-react";
+import React from "react";
+import { Form } from "./components/Form";
 
-export function Component() {
+function App() {
   return (
-    <form className="flex max-w-md flex-col gap-4">
-      <div>
-        <div className="mb-2 block">
-          <Label htmlFor="email1">Your email</Label>
-        </div>
-        <TextInput
-          id="email1"
-          type="email"
-          placeholder="name@flowbite.com"
-          required
-        />
-      </div>
-      <div>
-        <div className="mb-2 block">
-          <Label htmlFor="password1">Your password</Label>
-        </div>
-        <TextInput id="password1" type="password" required />
-      </div>
-      <div className="flex items-center gap-2">
-        <Checkbox id="remember" />
-        <Label htmlFor="remember">Remember me</Label>
-      </div>
-      <Button type="submit">Submit</Button>
-    </form>
+    <div>
+      <Form />
+    </div>
   );
 }
+
+export default App;
